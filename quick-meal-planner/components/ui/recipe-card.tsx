bc526a8764252aa52af0d30537fb054087a01836
Colton Leblond <62183336+coltcodess@users.noreply.github.com>
2026-03-25 21:24:43 -0400

    create temperory modal for recipe cards

--- quick-meal-planner/components/ui/recipe-card.tsx
+++ quick-meal-planner/components/ui/recipe-card.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Pressable, Modal, Alert } from "react-native";``
 import { Recipe } from "./types";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 
 type Props = {
   recipe: Recipe;
@@ -11,27 +13,48 @@ const RecipeCard: React.FC<Props> = ({ recipe, onPress }) => {
   const totalTime =
     recipe.prep_time_minutes + recipe.cook_time_minutes;
 
+  const [modalVisible, setModalVisible] = useState(false);
+// move recipe modal into own file
   return (
-    <TouchableOpacity style={styles.card} onPress={onPress}>
-      <View style={styles.content}>
-        <Text style={styles.title}>{recipe.name}</Text>
-
-        <Text style={styles.subtitle}>{recipe.cuisine}</Text>
-
-        <View style={styles.metaRow}>
-          <Text style={styles.meta}>⏱ {totalTime} min</Text>
-          <Text style={styles.meta}>🍽 {recipe.servings}</Text>
-        </View>
-
-        <View style={styles.tags}>
-          {recipe.tags.slice(0, 2).map((tag) => (
-            <Text key={tag} style={styles.tag}>
-              #{tag}
-            </Text>
-          ))}
+    <SafeAreaProvider>
+      
+      <Modal
+        animationType='slide'
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Recipe closed")
+          setModalVisible(!modalVisible);
+        }}
+      >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text>{recipe.name}</Text>
+          <Pressable
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text>Hide Modal</Text>
+          </Pressable>
         </View>
       </View>
-    </TouchableOpacity>
+      </Modal>
+      <Pressable style={styles.card} onPress={() => setModalVisible(true)}>
+        <View style={styles.content}>
+          <Text style={styles.title}>{recipe.name}</Text>
+          <Text style={styles.subtitle}>{recipe.cuisine}</Text>
+          <View style={styles.metaRow}>
+            <Text style={styles.meta}>⏱ {totalTime} min</Text>
+            <Text style={styles.meta}>🍽 {recipe.servings}</Text>
+          </View>
+          <View style={styles.tags}>
+            {recipe.tags.slice(0, 2).map((tag) => (
+              <Text key={tag} style={styles.tag}>
+                #{tag}
+              </Text>
+            ))}
+          </View>
+        </View>
+      </Pressable>
+    </SafeAreaProvider>
   );
 };
 
@@ -71,4 +94,24 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: "#007AFF",
   },
+  centeredView: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  }
 });
